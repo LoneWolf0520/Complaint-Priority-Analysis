963ea7dd3af49d8441cbb6b437407a409917725b
Lone Wolf <rabiasahar2005@gmail.com>
2026-06-21 10:08:07 +0500

    Added Complaint Classification

---
+++ script.js
@@ -0,0 +1,81 @@
+document.getElementById("analyzeBtn").addEventListener("click", function () {
+
+    let complaintText = document.getElementById("complaint").value.trim().toLowerCase();
+
+    let category = "General Inquiry";
+    let priority = "Low";
+    let recommendation = "Review and respond to the inquiry.";
+
+    if (
+        complaintText.includes("crash") ||
+        complaintText.includes("error") ||
+        complaintText.includes("bug") ||
+        complaintText.includes("website") ||
+        complaintText.includes("server")
+    ) {
+        category = "Technical Issue";
+        priority = "High";
+        recommendation = "Escalate immediately to technical support.";
+    }
+
+    else if (
+        complaintText.includes("payment") ||
+        complaintText.includes("refund") ||
+        complaintText.includes("billing") ||
+        complaintText.includes("invoice")
+    ) {
+        category = "Billing Issue";
+        priority = "Medium";
+        recommendation = "Forward complaint to billing department.";
+    }
+
+    else if (
+        complaintText.includes("service") ||
+        complaintText.includes("support") ||
+        complaintText.includes("staff") || 
+        complaintText.includes("assistance")
+    ) {
+        category = "Service Issue";
+        priority = "Medium";
+        recommendation = "Notify customer service manager for review.";
+    }
+
+    else if (
+        complaintText.includes("question") ||
+        complaintText.includes("information") ||
+        complaintText.includes("inquiry") ||
+        complaintText.includes("ask")
+    ) {
+        category = "General Inquiry";
+        priority = "Low";
+        recommendation = "Provide requested information to the customer.";
+    }
+
+    document.getElementById("result").innerHTML =
+        `
+        <h3>Analysis Result</h3>
+        <p><strong>Category:</strong> ${category}</p>
+        <p><strong>Priority:</strong> ${priority}</p>
+        <p><strong>Recommendation:</strong> ${recommendation}</p>
+        `;
+    let priorityClass = "";
+
+    if (priority === "High") {
+        priorityClass = "high";
+    }
+    else if (priority === "Medium") {
+        priorityClass = "medium";
+    }
+    else {
+        priorityClass = "low";
+    }
+
+    let newRow = `
+    <tr>
+        <td>${complaintText}</td>
+        <td>${category}</td>
+        <td class="${priorityClass}">${priority}</td>
+    </tr>
+    `;
+    document.getElementById("tableBody").innerHTML += newRow;
+});
